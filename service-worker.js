5988a44bcfaa6f1f7ab1ba324e5fcd1d0c737c49
hiltong <hiltong@live.com>
2018-08-25 07:32:42 -0500

    fetch1

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,7 @@ self.addEventListener('install', (event) => {
   self.addEventListener('activate', (event) => {
     console.log('updated service worker activated', event);
   });
+
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(new Response('arrrgh!'));
+  });
