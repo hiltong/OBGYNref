1bd33cbc4cf827b1db24c43f8eba7b0a3b0ae395
hiltong <hiltong@live.com>
2018-08-25 09:37:30 -0500

    cache10

--- service-worker.js
+++ service-worker.js
@@ -40,7 +40,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('mysite-static-v3').then(function(cache) {
         return cache.addAll([
-          
+          'index_ob.html',
           'index_gyn.html'
           // etc
         ]);
