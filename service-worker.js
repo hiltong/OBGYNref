3c5d08ab49ece951bbf4799be7e88cb32b403eb0
hiltong <hiltong@live.com>
2018-08-25 08:43:39 -0500

    cache3

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,8 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('version1').then((cache) => {
-        return cache.addAll(
-          [
-            '/index_ob.html',
-            '/index_gyn.html'
-          ]);
-      })
-    ); 
-  });
-
-
-/*self.addEventListener('install', (event) => {
     self.skipWaiting();
     console.log('updated service worker installed', event);
-  });*/
+  });
+  
   
   /*self.addEventListener('activate', (event) => {
     console.log('updated service worker activated', event);
