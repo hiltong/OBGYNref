7d44487d25db45e87dad41e6f5c3ab786880bf46
hiltong <hiltong@live.com>
2018-08-25 07:03:53 -0500

    sw2

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
-self.addEventListener('install', function(event) {
-  console.log('updated service worker installed', event);
+self.addEventListener('install', (event) => {
+    self.skipwaiting();
+    console.log('updated service worker installed', event);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('service worker activated', event);
 });
