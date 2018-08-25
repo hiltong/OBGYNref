c9da183214b5eb10ec3f4841bbe901b9246b985d
hiltong <hiltong@live.com>
2018-08-25 08:55:34 -0500

    cache4

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,39 @@
-self.addEventListener('install', (event) => {
+/*self.addEventListener('install', (event) => {
     self.skipWaiting();
     console.log('updated service worker installed', event);
+  });*/
+
+  
+  self.addEventListener('install', (event) => {
+
+    event.waitUntil(
+  
+      caches.open('version1').then((cache) => {
+  
+        return cache.addAll(
+  
+          [
+  
+            '/pirates.html',
+  
+            '/styles/pirates.css',
+  
+            '/styles/pirate.ttf',
+  
+            '/images/i-love-pirates.jpg'
+  
+          ]);
+  
+      })
+  
+    );
+  
   });
-  
-  
+
+
+
+
+
   /*self.addEventListener('activate', (event) => {
     console.log('updated service worker activated', event);
   });*/
