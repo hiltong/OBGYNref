99714e86dc4def143cd91c288ca4224412886484
hiltong <hiltong@live.com>
2018-08-25 08:29:45 -0500

    cache1

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
 self.addEventListener('install', (event) => {
     self.skipWaiting();
-    console.log('updated service worker installed', event);
+    if (!('caches' in self)) return;
+    event.waitUntil(
+      caches.open('version1').then((cache) => {
+        return cache.addAll(
+          [
+            '/index_ob.html',
+            '/index_gyn.html'
+          ]);
+      })
+    );
   });
+
+/*self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    console.log('updated service worker installed', event);
+  });*/
   
   self.addEventListener('activate', (event) => {
     console.log('updated service worker activated', event);
@@ -10,3 +24,4 @@ self.addEventListener('install', (event) => {
   /*self.addEventListener('fetch', (event) => {
     event.respondWith(new Response('arrrgh!'));
   });*/
+
