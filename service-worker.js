689692993ff3026a42d1401daf137fceff7f1e27
hiltong <hiltong@live.com>
2018-08-25 17:14:48 -0500

    cache 12

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,3 @@
-/*self.addEventListener('install', (event) => {
-    self.skipWaiting();
-    console.log('updated service worker installed', event);
-  });*/
-
-  
-/*var cacheName = 'app-shell-cache-v1';
-var filesToCache = ['/', '/index_ob.html'];
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-        return cache.addAll(filesToCache);
-    }).then(() => {
-      return self.skipWaiting();
-    })
-  );
-});*/
-
 self.addEventListener('install', event => {
     if(!('caches' in self)) return;
     event.waitUntil(
@@ -31,26 +13,13 @@ self.addEventListener('install', event => {
   });
 
 
- /* self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          'index_ob.html',
-          'index_gyn.html'
-          // etc
-        ]);
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
       })
     );
-  });*/
+  });
 
  
-
-
-  /*self.addEventListener('activate', (event) => {
-    console.log('updated service worker activated', event);
-  });*/
-
-  /*self.addEventListener('fetch', (event) => {
-    event.respondWith(new Response('arrrgh!'));
-  });*/
 
