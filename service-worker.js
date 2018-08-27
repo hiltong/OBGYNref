958556d911d920a945747b38f2ba62968735342d
hiltong <hiltong@live.com>
2018-08-26 21:02:23 -0500

    sunday7

--- service-worker.js
+++ service-worker.js
@@ -13,20 +13,20 @@ self.addEventListener('install', event => {
   });
 
 
-  /*self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });*/
-
   self.addEventListener('fetch', (event) => {
+
     event.respondWith(
-      caches.match(event.request).then((response) =>{
+  
+      caches.match(event.request).then((response) => {
+  
         return response || fetch(event.request);
+  
       })
+  
     );
+  
   });
+
+  
 
 
