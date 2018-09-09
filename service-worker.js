6642788e9d5a91f91f94d29f8581052a38542186
hiltong <hiltong@live.com>
2018-09-09 18:51:55 -0500

    2018-09-09-1850

--- service-worker.js
+++ service-worker.js
@@ -57,7 +57,7 @@ self.addEventListener('install', (event) => {
       caches.match(event.request).then((response) => {
         return response || fetch(event.request).then((response) => {
           console.log('fetched from network this time!');
-          return caches.open('v1').then((cache) => {
+          return caches.open(CURRENT_CACHE).then((cache) => {
             cache.put(event.request, response.clone());
             return response;
           });
