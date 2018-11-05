626ae091031958de6b6e4ce64a40d10a309ac14d
hiltong <hiltong@live.com>
2018-11-04 18:38:28 -0600

    2018-11-04-1838

--- script-aux.js
+++ script-aux.js
@@ -37,31 +37,48 @@ function SideBarClose() {
 
 
 function AccToggle(accID) {
-  var chevronId = accID+ "_c";
+  var chevronId = accID + "_c";
   var x = document.getElementById(accID);
   var chevron = document.getElementById(chevronId);
 
   if (x.style.display == "block") {
     // chevron.innerHTML='&#9660';
-    chevron.className =ARROWDOWN;
+    chevron.className = ARROWDOWN;
     x.style.display = "none";
     sessionStorage.setItem(accID, 'none')
   }
-  else {  
+  else {
     // chevron.innerHTML='&#9650';
-    chevron.className=ARROWUP;
+    chevron.className = ARROWUP;
     x.style.display = "block";
     sessionStorage.setItem(accID, 'block')
   }
   var data = sessionStorage.getItem(accID);
 }
 
-const ARROWRIGHT='fa fa-chevron-right';
-const ARROWDOWN='fa fa-chevron-down';
-const ARROWUP='fa fa-chevron-up';
+const TITLEHTML = "Ob/Gyn Pocket Notess <sup>r</sup>";
+const HAMBURGER = 'fa fa-bars';
+const BACKARROW = 'fa fa-arrow-left';
+const ARROWRIGHT = 'fa fa-chevron-right';
+const ARROWDOWN = 'fa fa-chevron-down';
+const ARROWUP = 'fa fa-chevron-up';
 
 //Sets divs for accordian and algorithms on page
-function SetAccDivsALgoDivs(){
+function SetAccDivsALgoDivs() {
+
+  var bbarray = document.getElementsByClassName("hg-BackButton");
+  var bbarrayi = bbarray[0].getElementsByTagName("i");
+  bbarrayi[0].className = BACKARROW;
+
+  var titlecarray = document.getElementsByClassName("hg-NavBarTitle");
+  var titlespan = titlecarray[0];
+  titlespan.innerHTML =TITLEHTML;
+
+  var hbarray = document.getElementsByClassName("hg-HamBtn");
+  var hbarrayi = hbarray[0].getElementsByTagName("i");
+  hbarrayi[0].className = HAMBURGER;
+
+
   var accArray = document.getElementsByClassName("hg-accDivMain");
   for (i = 0; i < accArray.length; i++) {
     var accdiv = accArray[i];
@@ -69,48 +86,40 @@ function SetAccDivsALgoDivs(){
     // var chevronId = accdiv.id+1;
     var chevronId = accdiv.id + "_c";
     var chevron = document.getElementById(chevronId);
-    if (data=='block'){
+    if (data == 'block') {
       //  chevron.innerHTML='&#9650';
 
-       chevron.className =ARROWUP;
-      accdiv.style.display="block";
+      chevron.className = ARROWUP;
+      accdiv.style.display = "block";
     }
-    else{
-      accdiv.style.display="none";
+    else {
+      accdiv.style.display = "none";
     }
   }
   var algoArray = document.getElementsByClassName("hg-algoDivMain");
   for (i = 0; i < algoArray.length; i++) {
     var algodiv = algoArray[i];
     var data = sessionStorage.getItem(algodiv.id);
-    if (data=='block'){
-      algodiv.style.display="block";
+    if (data == 'block') {
+      algodiv.style.display = "block";
     }
-    else if (data=='none'){
-      algodiv.style.display="none";
+    else if (data == 'none') {
+      algodiv.style.display = "none";
     }
-    else if (i==0){
-      algodiv.style.display="block";
+    else if (i == 0) {
+      algodiv.style.display = "block";
     }
   }
 
   var NavLinkArray = document.getElementsByClassName("hg-NavLink");
-  for (i = 0; i <NavLinkArray.length; i++) {
+  for (i = 0; i < NavLinkArray.length; i++) {
     var nla = NavLinkArray[i];
-    var iArray =  nla.getElementsByTagName("i");
-    iArray[0].className=ARROWRIGHT;
-  } 
+    var iArray = nla.getElementsByTagName("i");
+    iArray[0].className = ARROWRIGHT;
+  }
 
 }
 
-function SetNavLinkChevron(){
-  var NavLinkArray = document.getElementsByClassName("hg-NavLink");
-  for (i = 0; i <NavLinkArray.length; i++) {
-    var nla = NavLinkArray[i];
-    var iArray =  nla.getElementsByTagName("i");
-    iArray[0].className=ARROWRIGHT;
-  } 
-}
 
 
 /*Algorhithm-------------------------------------------------------------------------------*/
@@ -122,19 +131,19 @@ function AlgoNext(idhide, idshow) {
   sessionStorage.setItem(idshow, 'block')
 }
 
-function SetAlgoDivs(){
+function SetAlgoDivs() {
   var algoArray = document.getElementsByClassName("hg-algoDivMain");
   for (i = 0; i < algoArray.length; i++) {
     var algodiv = algoArray[i];
     var data = sessionStorage.getItem(algodiv.id);
-    if (data=='block'){
-      algodiv.style.display="block";
+    if (data == 'block') {
+      algodiv.style.display = "block";
     }
-    else if (data=='none'){
-      algodiv.style.display="none";
+    else if (data == 'none') {
+      algodiv.style.display = "none";
     }
-    else if (i==0){
-      algodiv.style.display="block";
+    else if (i == 0) {
+      algodiv.style.display = "block";
     }
 
   }
